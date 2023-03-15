72f8e3e5d8f41b7967349f931f22b7665d6b60d1
Cristian René Escobar <profescobarcristian@gmail.com>
2023-03-15 17:56:41 -0300

    Segundo Commit - Ejercicio7

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,6 +18,36 @@ public class Ejercicio7 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean menu=true;
+        int contC=0,contI=0;
+        String cadena;
+        Scanner leer=new Scanner(System.in);
+              
+        
+        do {
+        System.out.println("Dispositivo RS232");
+        System.out.println("Escriba la cadena");
+        cadena=leer.nextLine();
+               
+        
+        if(cadena.length()==5 && cadena.substring(0,1).equalsIgnoreCase("x")&&cadena.substring(4,5).equalsIgnoreCase("o")){
+           System.out.println("Cadena CORRECTA");
+            contC=contC+1;
+            System.out.println("=================");
+           }else if (cadena.equalsIgnoreCase("&&&&&")){
+               menu=false;
+           }else{
+           System.out.println("La Cadena ES INCORRECTA");
+           contI=contI+1;
+           System.out.println("=================");
+            }
+        } while (menu==true);
+   
+        //MENSAJE FINAL
+        System.out.println("La cantidad de cadenas CORRECTAS son: "+contC);
+        System.out.println("La cantidad de cadenas INCORRECTAS son: "+contI);
+         System.out.println("=================");
+         System.out.println("PROGRAMA TERMIADO");
+         System.out.println("=================");        
+        }    
     }
-    
-}
